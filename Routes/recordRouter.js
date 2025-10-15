02791b7ee3c2eccdee2c057a81df915e3473302a
esha habib <habibesha0@gmail.com>
2025-10-14 23:43:53 -0700

    latest code

--- Routes/recordRouter.js
+++ Routes/recordRouter.js
@@ -31,7 +31,7 @@ const {
   getRecords,     // ✅ list with pagination + search
   getRecordById,  // ✅ single record
   deleteRecord,
-} = require("../Controller/recordController");
+} = require("../Controller/RecordController");
 
 const router = express.Router();
 
